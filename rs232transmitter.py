4ae25e4fc3d0c4cc13341afb271f90cc622af2a6
lesterwilliam <adrian.schwizgebel@gmail.com>
2018-04-03 13:00:40 +0200

    first after copy

--- rs232transmitter.py
+++ rs232transmitter.py
@@ -1,1 +1,24 @@
 #RS232 transmitter
+
+import serial
+import time
+
+port = "/dev/ttyAMA0"
+#port = "/dev/ttyS0"
+
+def readLine(port):
+    s = ""
+    while True:
+        ch = port.read()
+        s += ch
+        if ch == '\r':
+            return s
+
+ser = serial.Serial(port, baudrate = 1200)
+print "starting"
+while True:
+    time.sleep(1)
+    print "sending synch"
+    ser.write("A")
+    rcv = readLine(ser)
+    print "received:", rcv
