6a454689856b2f92d348b5e674b7c544c4c8e423
lesterwilliam <adrian.schwizgebel@gmail.com>
2018-04-03 14:48:53 +0200

    Simple messages imbedded

--- rs232transmitter.py
+++ rs232transmitter.py
@@ -5,14 +5,23 @@ import serial
 port = "COM1"
 baud = 1200
 timeout = 1
- 
+inputMSG = "Please enter value to send: "
+
 ser = serial.Serial(port, baud, timeout=1)
 
 # open the serial port
 if ser.isOpen():
-	print(ser.name + ' is open...')
-	val = 0b1
-	ser.write(b'2')
-	ser.close()
-	print('Sent ' + str(val) + ' to ' + str(ser.name))
+	print(ser.name + ' is open!')
+else:
+	print('Could not open serial port ' + str(ser.name) + '!')
 	exit()
+
+# endless loop, listens to command line and writes to serial port
+while True:
+	val = input(inputMSG)
+	if val == "exit":
+		ser.close()
+		print('Closed serial communication!')
+		exit()
+	ser.write(b'50')
+	print('Sent "' + str(val) + '" to ' + str(ser.name))
