4c95a752302a3c3c0bcb4df5f8d289f865faeef8
lesterwilliam <adrian.schwizgebel@gmail.com>
2018-04-03 14:22:19 +0200

    First correct byte sent

--- rs232transmitter.py
+++ rs232transmitter.py
@@ -4,18 +4,15 @@ import serial
  
 port = "COM1"
 baud = 1200
+timeout = 1
  
 ser = serial.Serial(port, baud, timeout=1)
-    # open the serial port
+
+# open the serial port
 if ser.isOpen():
-     print(ser.name + ' is open...')
- 
-while True:
-    cmd = input("Enter command or 'exit':")
-    if cmd == 'exit':
-        ser.close()
-        exit()
-    else:
-        ser.write(cmd.encode('ascii')+'\r\n')
-        out = ser.read()
-        print('Receiving...'+out)
+	print(ser.name + ' is open...')
+	val = 0b1
+	ser.write(b'2')
+	ser.close()
+	print('Sent ' + str(val) + ' to ' + str(ser.name))
+	exit()
