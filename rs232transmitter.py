7edeaf3a7f9a80168ce4119303c825144c9a3498
lesterwilliam <adrian.schwizgebel@gmail.com>
2018-04-04 08:38:19 +0200

    Bug detected: Some bytes are more equal than others.

--- rs232transmitter.py
+++ rs232transmitter.py
@@ -1,22 +1,23 @@
 #RS232 transmitter
 
+#pyserial module
 import serial
-import struct
  
 port = "COM1"
 baud = 1200
 timeout = 1
 inputMSG = "Please enter value to send: "
 
+# create serial object from class
 ser = serial.Serial(port, baud, timeout=1)
-
+	
 # open the serial port
 if ser.isOpen():
 	print(ser.name + ' is open!')
 else:
 	print('Could not open serial port ' + str(ser.name) + '!')
 	exit()
-
+	
 # endless loop, listens to command line and writes to serial port
 while True:
 	val = input(inputMSG)
@@ -24,6 +25,21 @@ while True:
 		ser.close()
 		print('Closed serial communication!')
 		exit()
-	val = val.encode('utf-8')
+	val = int(val.encode('utf-8'))
+	#ser.write(val.encode('utf-8'))
 	ser.write(val)
+	print('Sent "' + str(val) + '" to ' + str(ser.name))
+	
+	
+	
+	
+# endless loop, listens to command line and writes to serial port
+while True:
+	val = input(inputMSG)
+	if val == "exit":
+		ser.close()
+		print('Closed serial communication!')
+		exit()
+	val = chr(int(val))
+	ser.write(val.encode('utf-8'))
 	print('Sent "' + str(val) + '" to ' + str(ser.name))
