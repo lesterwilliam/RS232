5e19b6c32c467b777e8e61fa28f9e75c963d201e
lesterwilliam <adrian.schwizgebel@gmail.com>
2018-04-04 12:30:00 +0200

    New file

--- rs232transmitter2.py
+++ rs232transmitter2.py
@@ -10,22 +10,80 @@ from colorama import Fore, Back, Style
 defaultport = "COM1"
 defaultbaud = 1200
 timeout = 1
-inputMSG = "Please enter value to send: "
 
+def OpenConnection():
+	ser = serial.Serial(definedport, definedbaud, timeout=1)
+	if ser.isOpen():
+		print(Fore.GREEN + ser.name + ' is open!\n' + Style.RESET_ALL)
+	else:
+		print('\nCould not open serial port ' + str(ser.name) + '!\n')
+		exit()
+	#Standby()
+	
+def SendByte():
+	clear()
+	val = input('Please enter integer to send.')
+	if not val.isdigit():
+		print(Fore.RED + '\nPlease only input integer between 0 and 255 or command.\n' + Style.RESET_ALL)
+	elif int(val) < 0 or int(val) > 255:
+		print(Fore.RED + '\nPlease only input integer between 0 and 255 or command.\n' + Style.RESET_ALL)
+	else:
+		valBytearray = ([int(val)])
+		ser.write(valBytearray)
+		print(Fore.GREEN + 'Sent "' + str(val) + '" to ' + str(ser.name) + '.\n' + Style.RESET_ALL)
+		
+def Standby():
+	clear()
+	print('Enter command or use <help>')
+	
+def OpenHelp():
+	clear()
+	print('Insert decimal value between 0 and 255 and press enter.\n')
+	print('Type <baud> to change baud rate.\n')
+	print('Type <exit> to close program.\n')
+	input('Quit help with any key.')
+	clear()
+
+def Exit():
+	ser.close()
+	print(Fore.YELLOW + '\nClosed serial communication!' + Style.RESET_ALL)
+	clear()
+	exit()
+	
+def CloseConnection():
+	clear()
+	ser.close()
+	print(Fore.YELLOW + 'Closed serial communication!' + Style.RESET_ALL)
+	
+#def ChangeBaud():
+#	clear()
+#	ser.close()
+#	print(Fore.RED + 'Closed serial communication!' + Style.RESET_ALL)
+#	definedbaud = input('Please enter new baud rate: ')
+#	ser = serial.Serial(definedport, definedbaud, timeout=1)
+#	# open the serial port
+#	if ser.isOpen():
+#		print(Fore.GREEN + ser.name + ' is open!\n' + Style.RESET_ALL)
+#	else:
+#		print('\nCould not open serial port ' + str(ser.name) + '!\n')
+#		exit()
+	
+	
+	
 # clear cmd console
 clear = lambda: os.system('cls')
 clear()
 
+# setup routines
 # define port
 definedport = input('Please enter serial port. Leave blank for default port COM1.')
 if definedport == "":
 	definedport = defaultport
-	
+
 # define baud rate
 definedbaud = input('Please enter baud rate. Leave blank for default rate 1200.')
 if definedbaud == "":
 	definedbaud = defaultbaud
-
 # create serial object from class
 ser = serial.Serial(definedport, definedbaud, timeout=1)
 
@@ -39,32 +97,26 @@ else:
 # endless loop, listens to command line and writes to serial port
 while True:
 	# read input and save it as unicode into val
-	val = input(inputMSG)
+	val = input("Please enter value to send or command: ")
 	
 	# exit condition
 	if val == "exit":
-		ser.close()
-		print(Fore.YELLOW + '\nClosed serial communication!' + Style.RESET_ALL)
-		clear()
-		exit()
+		CloseConnection()
+		Exit()
+	elif val == "openconnection":
+		OpenConnection()
+
 	# close connection
 	elif val == "close":
-		clear()
-		ser.close()
-		print(Fore.YELLOW + 'Closed serial communication!' + Style.RESET_ALL)
+		CloseConnection()
+
 	# open help
 	elif val == "help":
-		clear()
-		print('Insert decimal value between 0 and 255 and press enter.\n')
-		print('Type <baud> to change baud rate.\n')
-		print('Type <exit> to close program.\n')
-		input('Quit help with any key.')
-		clear()
+		OpenHelp()
+
 	# change baud
 	elif val == "baud":
-		clear()
-		ser.close()
-		print(Fore.RED + 'Closed serial communication!' + Style.RESET_ALL)
+		CloseConnection()
 		definedbaud = input('Please enter new baud rate: ')
 		ser = serial.Serial(definedport, definedbaud, timeout=1)
 		# open the serial port
@@ -73,11 +125,12 @@ while True:
 		else:
 			print('\nCould not open serial port ' + str(ser.name) + '!\n')
 			exit()
+
 	# wrong input contition
 	elif not val.isdigit():
-		print(Fore.RED + '\nPlease only input integer between 0 and 255 or <exit> command.\n' + Style.RESET_ALL)
+		print(Fore.RED + '\nPlease only input integer between 0 and 255 or command.\n' + Style.RESET_ALL)
 	elif int(val) < 0 or int(val) > 255:
-		print(Fore.RED + '\nPlease only input integer between 0 and 255 or <exit> command.\n' + Style.RESET_ALL)
+		print(Fore.RED + '\nPlease only input integer between 0 and 255 or command.\n' + Style.RESET_ALL)
 	else:
 		# put val into a bytearray, integer-cast
 		valBytearray = ([int(val)])
