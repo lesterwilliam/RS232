68c39cd430d0b4b932317eed5f80054bdc8f1559
lesterwilliam <adrian.schwizgebel@gmail.com>
2018-04-04 11:16:35 +0200

    Finaly

--- rs232transmitter.py
+++ rs232transmitter.py
@@ -2,20 +2,38 @@
 
 #pyserial module
 import serial
- 
-port = "COM1"
-baud = 1200
+import os
+from colorama import init
+init()
+from colorama import Fore, Back, Style
+
+defaultport = "COM1"
+defaultbaud = 1200
 timeout = 1
 inputMSG = "Please enter value to send: "
 
-# create serial object from class
-ser = serial.Serial(port, baud, timeout=1)
+# clear cmd console
+clear = lambda: os.system('cls')
+clear()
+
+# define port
+definedport = input('Please enter serial port. Leave blank for default port COM1.')
+if definedport == "":
+	definedport = defaultport
 	
+# define baud rate
+definedbaud = input('Please enter baud rate. Leave blank for default rate 1200.')
+if definedbaud == "":
+	definedbaud = defaultbaud
+
+# create serial object from class
+ser = serial.Serial(definedport, definedbaud, timeout=1)
+
 # open the serial port
 if ser.isOpen():
-	print(ser.name + ' is open!')
+	print(Fore.GREEN + ser.name + ' is open!\n' + Style.RESET_ALL)
 else:
-	print('Could not open serial port ' + str(ser.name) + '!')
+	print('\nCould not open serial port ' + str(ser.name) + '!\n')
 	exit()
 
 # endless loop, listens to command line and writes to serial port
@@ -26,12 +44,44 @@ while True:
 	# exit condition
 	if val == "exit":
 		ser.close()
-		print('Closed serial communication!')
+		print(Fore.YELLOW + '\nClosed serial communication!' + Style.RESET_ALL)
+		clear()
 		exit()
-	
-	# put val into a bytearray, integer-cast
-	valBytearray = ([int(val)])
-	
-	# write complete array to serial port
-	ser.write(valBytearray)
-	print('Sent "' + str(val) + '" to ' + str(ser.name))
+	# close connection
+	elif val == "close":
+		clear()
+		ser.close()
+		print(Fore.YELLOW + 'Closed serial communication!' + Style.RESET_ALL)
+	# open help
+	elif val == "help":
+		clear()
+		print('Insert decimal value between 0 and 255 and press enter.\n')
+		print('Type <baud> to change baud rate.\n')
+		print('Type <exit> to close program.\n')
+		input('Quit help with any key.')
+		clear()
+	# change baud
+	elif val == "baud":
+		clear()
+		ser.close()
+		print(Fore.RED + 'Closed serial communication!' + Style.RESET_ALL)
+		definedbaud = input('Please enter new baud rate: ')
+		ser = serial.Serial(definedport, definedbaud, timeout=1)
+		# open the serial port
+		if ser.isOpen():
+			print(Fore.GREEN + ser.name + ' is open!\n' + Style.RESET_ALL)
+		else:
+			print('\nCould not open serial port ' + str(ser.name) + '!\n')
+			exit()
+	# wrong input contition
+	elif not val.isdigit():
+		print(Fore.RED + '\nPlease only input integer between 0 and 255 or <exit> command.\n' + Style.RESET_ALL)
+	elif int(val) < 0 or int(val) > 255:
+		print(Fore.RED + '\nPlease only input integer between 0 and 255 or <exit> command.\n' + Style.RESET_ALL)
+	else:
+		# put val into a bytearray, integer-cast
+		valBytearray = ([int(val)])
+		
+		# write complete array to serial port
+		ser.write(valBytearray)
+		print(Fore.GREEN + 'Sent "' + str(val) + '" to ' + str(ser.name) + '.\n' + Style.RESET_ALL)
