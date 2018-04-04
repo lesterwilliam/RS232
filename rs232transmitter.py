cebeb1020473948f4ed774168db9ffad46d5e970
lesterwilliam <adrian.schwizgebel@gmail.com>
2018-04-04 15:59:09 +0200

    Totally done and finished. Probably still not but hey.

--- rs232transmitter.py
+++ rs232transmitter.py
@@ -3,9 +3,11 @@
 #pyserial module
 import serial
 import os
+import time
 from colorama import init
 init()
 from colorama import Fore, Back, Style
+from random import randint
 
 clear = lambda: os.system('cls')
 clear()
@@ -17,13 +19,15 @@ timeout = 1
 ser = serial.Serial(DefaultPort, DefaultBaud, timeout=1)
 
 def Init():
+	print('RS232 transmitter by Adrian Schwizgebel\n_______________________________________\n\n')
 	PortSel()
 	BaudSel()
 	ser.close()
 	Standby()
 
 def Standby():
-	KeyboardInput = input('Enter command or use <help>')
+	print('Enter command or use ' + Fore.GREEN + '<help>' + Style.RESET_ALL + ': ')
+	KeyboardInput = input()
 	
 	if KeyboardInput == 'help':
 		OpenHelp()
@@ -36,19 +40,20 @@ def Standby():
 				print(Fore.GREEN + ser.name + ' is open!\n' + Style.RESET_ALL)
 			else:
 				print('\nCould not open serial port ' + str(ser.name) + '!\n')
-		input()
 		Standby()
 	elif KeyboardInput == 'close':
-		clear()
 		if ser.isOpen():
 			ser.close()
 			print(Fore.YELLOW + 'Closed serial communication!' + Style.RESET_ALL)
 		else:
 			print('There was no connection.')
-		input()
 		Standby()
 	elif KeyboardInput == 'send':
+		clear()
 		ByteWrite()
+	elif KeyboardInput == 'random':
+		Random()
+		Standby()
 	elif KeyboardInput == 'port':
 		PortSel()
 		Standby()
@@ -61,41 +66,44 @@ def Standby():
 		exit()
 	elif KeyboardInput == '':
 		Standby()
+	else:
+		Standby()
 	
 def OpenHelp():
 	clear()
 	print('This program is designed to write single bytes to a RS232 port.\n')
 	print('Type ' + Fore.GREEN + '<open>' + Style.RESET_ALL + ' to open a serial connection.')
 	print('Type ' + Fore.GREEN + '<close>' + Style.RESET_ALL + ' to close a serial connection.')
-	print('Type ' + Fore.GREEN + '<send>' + Style.RESET_ALL + ' to send data.')
+	print('Type ' + Fore.GREEN + '<send>' + Style.RESET_ALL + ' to send byte.')
+	print('Type ' + Fore.GREEN + '<random>' + Style.RESET_ALL + ' to send random byte.')
 	print('Type ' + Fore.GREEN + '<port>' + Style.RESET_ALL + ' to change serial port.')
 	print('Type ' + Fore.GREEN + '<baud>' + Style.RESET_ALL + ' to change baudrate.')
 	print('Type ' + Fore.GREEN + '<exit>' + Style.RESET_ALL + ' to close program.')
-	input('\nQuit help with any key.')
+	input('\nQuit help with any key.\n\n')
 	Standby()
 	
 def PortSel():
-	temp = input('Please enter serial port. Leave blank for default port COM1.')
+	temp = input('Please enter serial port. Leave blank for default port COM1: ')
 	if temp == '':
 		ser.port = DefaultPort
-		print('Default port ' + Fore.GREEN + 'COM1 ' + Style.RESET_ALL + 'chosen.\n')
+		print('Set to default port ' + Fore.GREEN + 'COM1 ' + Style.RESET_ALL + '.\n')
 	else:
 		ser.port = temp
-		print('Port ' + Fore.GREEN + ser.port + Style.RESET_ALL + ' chosen.')
+		print('Set port to ' + Fore.GREEN + ser.port + Style.RESET_ALL + '.')
 		
 def BaudSel():
-	temp = input('Please enter baudrate. Leave blank for default baudrate of 1200.')
+	temp = input('Please enter baudrate. Leave blank for default baudrate of 1200: ')
 	if temp == '':
 		ser.baudrate = DefaultBaud
-		print('Default baudrate of ' + Fore.GREEN + '1200 ' + Style.RESET_ALL + 'chosen.\n')
+		print('Set to default baudrate of ' + Fore.GREEN + '1200 ' + Style.RESET_ALL + '.\n')
 	else:
 		ser.baudrate = int(temp)
-		print('Baudrate ' + Fore.GREEN + str(ser.baudrate) + Style.RESET_ALL + ' chosen.')
+		print('Set Baudrate to ' + Fore.GREEN + str(ser.baudrate) + Style.RESET_ALL + '.')
 		
 def ByteWrite():
-	clear()
-	val = input('Please enter value to send!')
+	val = input('Please enter value to send: ')
 	if val == 'menu' or val == 'back' or val == 'close' or val == 'exit':
+		clear()
 		Standby()
 	elif not val.isdigit():
 		print(Fore.RED + '\nPlease only input integer between 0 and 255 or command.\n' + Style.RESET_ALL)
@@ -111,6 +119,16 @@ def ByteWrite():
 			ByteWrite()
 		else:
 			print(Fore.RED + 'There is no open connection!' + Style.RESET_ALL)
-			input()
 			Standby()
+			
+def Random():
+	temp = randint(0,255)
+	valBytearray2 = ([temp])
+	if ser.isOpen():
+		ser.write(valBytearray2)
+		Standby()
+	else:
+		print(Fore.RED + 'There is no open connection!' + Style.RESET_ALL)
+		Standby()
+	
 Init()
